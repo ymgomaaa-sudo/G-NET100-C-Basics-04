12a281ff9e5fb36ed8a9b38c2531aefe53a04e22
ALBORAQ <ALBORAQ@DESKTOP-59J143T>
2026-09-16 15:30:45 +0300

    Q11

--- Assignment-2/Program.cs
+++ Assignment-2/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Drawing;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Assignment_2
 {
@@ -81,27 +83,38 @@ namespace Assignment_2
             //    Console.WriteLine("You can borrow this book");
             //}
             //#endregion
-            #region Q10
+            //#region Q10
             //Given string title = "Refactoring";, write a switch statement that prints "Great choice!" if the title is "Clean Code", "Nice pick!" if it's "Refactoring", and "Never heard of it" for anything else. 
-            string title = "Refactoring";
+            //string title = "Refactoring";
+            //switch (title)
+            //{
+            //    case "Clean Code":
+            //        Console.WriteLine("Great choice!");
+            //        break;
+            //    case "Refactoring":
+            //        Console.WriteLine("Nice pick!");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Never heard of it");
+            //        break;
+            //}
+            //#endregion
+            //#region Q10
+            ////Given int pages = 464;, use the ternary operator to store "Long Book" or "Short Book" into a variable sizeLabel(same rule as question 8: long if pages > 300).
+            //int pages = 464;
 
-            switch (title)
+            //string sizeLabel = pages > 300 ? "Long Book" : "Short Book";
+
+            //Console.WriteLine(sizeLabel);
+            //#endregion
+            #region Q11
+            //Given string[] books = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };, use a for loop to print each book with its position number, like 1.Clean Code.
+            string[] books = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };
+            for (int i = 0; i < books.Length; i++)
             {
-                case "Clean Code":
-                    Console.WriteLine("Great choice!");
-                    break;
-
-                case "Refactoring":
-                    Console.WriteLine("Nice pick!");
-                    break;
-
-                default:
-                    Console.WriteLine("Never heard of it");
-                    break;
+                Console.WriteLine($"{i + 1}. {books[i]}");
             }
             #endregion
-
-
         }
     }
 }
