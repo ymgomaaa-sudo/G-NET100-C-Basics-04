42395b3d46b2af0ff01fe709d931782836f647ee
ALBORAQ <ALBORAQ@DESKTOP-59J143T>
2026-09-16 15:41:14 +0300

    Q13

--- Assignment-2/Program.cs
+++ Assignment-2/Program.cs
@@ -125,16 +125,17 @@ namespace Assignment_2
             //    i++;
             //}
             //#endregion
-            #region Q13
-            //Write a do-while loop that prints "Checking book..." exactly 3 times. 
-            int count = 0;
-            do
-            {
-                Console.WriteLine("Checking book...");
-                count++;
-            }
-            while (count < 3);
-            #endregion
+            //#region Q13
+            ////Write a do-while loop that prints "Checking book..." exactly 3 times. 
+            //int count = 0;
+            //do
+            //{
+            //    Console.WriteLine("Checking book...");
+            //    count++;
+            //}
+            //while (count < 3);
+            //#endregion
+
         }
     }
 }
